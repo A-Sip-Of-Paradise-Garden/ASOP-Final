f12b44871495fca93aab1a0c0e281df7b8d8e265
bavichugo <victorhugobatistela@gmail.com>
2023-09-23 12:45:29 -0400

    Add members page, admin type, and membership status

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,13 +3,14 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+// ASOP Draft
 const firebaseConfig = {
-  apiKey: "AIzaSyDGkqYC-fo6LbpMFPP9fi5caS99GMnVAxM",
-  authDomain: "asop-final.firebaseapp.com",
-  projectId: "asop-final",
-  storageBucket: "asop-final.appspot.com",
-  messagingSenderId: "752957988415",
-  appId: "1:752957988415:web:361de51c87ed267ceea28a"
+  apiKey: "AIzaSyACLrwV5qyCBr1OIKg4WeG91LG_uceTKTQ",
+  authDomain: "asop-test.firebaseapp.com",
+  projectId: "asop-test",
+  storageBucket: "asop-test.appspot.com",
+  messagingSenderId: "460234170888",
+  appId: "1:460234170888:web:c9b5a3d1f5535de04227bd"
 };
 
 const app = initializeApp(firebaseConfig);
