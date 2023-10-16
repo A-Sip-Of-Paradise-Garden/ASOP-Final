3c0bd6f9904933a718724836a6f739b4ac809923
Nathnael Fekade <86081492+Nati-Fekade@users.noreply.github.com>
2023-10-16 00:02:43 -0400

    Added About Us Section
    I added the basic structure of the "About Us" section on the landing (home) page but the CSS needs more work.

---
+++ src/pages/Home/About.js
@@ -0,0 +1,45 @@
+import React from "react";
+import asop_Keyatta_img1 from "../../assets/home/asop_Keyatta_img1.png";
+import asop_Rori_img1 from "../../assets/home/asop_Rori_img1.png";
+import asop_Stephanie_img1 from "../../assets/home/asop_Stephanie_img1.png";
+
+const About = () => {
+    const AboutInfo = [
+        {
+            image: asop_Keyatta_img1,
+            title: "Keyatta Mincey Parker",
+            text: "Executive Director",
+        },
+        {
+            image: asop_Rori_img1,
+            title: "Rori Robinson",
+            text: "Director of Gardening and Development",
+        },
+        {
+            image: asop_Stephanie_img1,
+            title: "Stephanie R Saputo",
+            text: "Director of Communications",
+        },
+    ];
+    return (
+        <div className="about-section-wrapper">
+            <div className="about-section-top">
+                <h1 className="primary-heading">About Us</h1>
+            </div>
+            
+            <div className="about-section-bottom">
+                {AboutInfo.map((data) => (
+                    <div className="about-section-info" key={data.title}>
+                        <div className="info-boxes-img-container">
+                            <img src={data.image} alt="" />
+                        </div>
+                        <h2>{data.title}</h2>
+                        <p>{data.text}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default About;
