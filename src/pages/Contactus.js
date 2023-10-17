c42c003a322aa1941fb71b46c743dfe4ea4dfb29
jonathanvo18 <jonathan.vo18@yahoo.com>
2023-10-16 21:26:48 -0400

    Contact Facebook page
    Added FB link to Contact page

--- src/pages/Contactus.js
+++ src/pages/Contactus.js
@@ -4,12 +4,17 @@ const sendEmail = () => {
   window.location.href = 'mailto:${asipofparadisegarden@gmail.com}';
 }
 
+const redirectToFacebook = () => {
+  window.location.href = 'https://www.facebook.com/asipofparadisegarden/';
+}
+
 const Contact = () => {
     return (
       <div>
         <h1>Contact Us</h1>
-        
+
       <button onClick={sendEmail}>Send us an Email</button>
+      <button onClick={redirectToFacebook}>Visit Our Facebook Page</button>
         </div>
     )
   }
