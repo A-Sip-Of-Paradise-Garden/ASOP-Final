550803c839608521ce0f016a123a131ae92a0d93
jonathanvo18 <jonathan.vo18@yahoo.com>
2023-10-17 00:25:01 -0400

    Fixed buttons

--- src/pages/Contactus.js
+++ src/pages/Contactus.js
@@ -17,20 +17,19 @@ const Contact = () => {
       <div>
         <p align="center">
         <h1 className ="primary-heading">Contact Us</h1>
-        </p>
 
-        <button onClick={sendEmail}>Send us an Email</button>
+        <button className="secondary-button" onClick={sendEmail}>Send us an Email</button>
         <br />
 
-        <p align="center">
         <h1 className ="primary-heading">Socials:</h1>
-        </p>
-        <button onClick={redirectToFacebook}>Visit Our Facebook Page</button>
-        <button onClick={redirectToIG}>Visit Our Instagram Page</button>
+
+        <button className="secondary-button" onClick={redirectToFacebook}>Visit Our Facebook Page</button>
+        <br />
+        <button className="secondary-button" onClick={redirectToIG}>Visit Our Instagram Page</button>
         <br />
 
         
-        <p align="center">
+
         <h1 className ="primary-heading">Get Directions</h1>
         <iframe 
           src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d3317.858194057881!2d-84.34714527283595!3d33.73848283423007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e0!4m3!3m2!1d33.7384805!2d-84.3444067!4m5!1s0x88f5015cbf3101fd%3A0xf5cd8a395657ddb0!2s572%20Stokeswood%20Ave%20SE%2C%20Atlanta%2C%20GA%2030316!3m2!1d33.7384449!2d-84.3444067!5e0!3m2!1sen!2sus!4v1697506734263!5m2!1sen!2sus" 
