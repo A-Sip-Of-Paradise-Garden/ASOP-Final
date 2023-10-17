f8895c3364e46b1ba1f70867661987655c9b79df
jonathanvo18 <jonathan.vo18@yahoo.com>
2023-10-16 21:33:49 -0400

    Contact IG Page
    created a new link leading to the asop instagram page

--- src/pages/Contactus.js
+++ src/pages/Contactus.js
@@ -8,13 +8,20 @@ const redirectToFacebook = () => {
   window.location.href = 'https://www.facebook.com/asipofparadisegarden/';
 }
 
+const redirectToIG = () => {
+  window.location.href = 'https://www.instagram.com/Asipofparadisegarden';
+}
+
 const Contact = () => {
     return (
       <div>
         <h1>Contact Us</h1>
 
       <button onClick={sendEmail}>Send us an Email</button>
+      <br />
+      <h1>Socials</h1>
       <button onClick={redirectToFacebook}>Visit Our Facebook Page</button>
+      <button onClick={redirectToIG}>Visit Our Instagram Page</button>
         </div>
     )
   }
