e7975e5ad6a986ea5bf90292cef032baf296088d
vanto-dev <acdevita19@gmail.com>
2023-11-13 01:23:00 -0500

    Added firebase dues update

--- src/pages/DuesPaymentSuccess.js
+++ src/pages/DuesPaymentSuccess.js
@@ -1,16 +1,50 @@
 import asopImage from "../assets/home/asop_img3.png";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { UserAuth } from "../context/AuthContext";
+import { db } from "../config/firebase";
+import { doc, updateDoc } from "firebase/firestore";
+import "../App.css"
 
-const DuesPaymentSuccess = () => (
-    <section>
-        <h1>Thanks For Paying Your Dues!</h1>
-        <div className={"align-center"}>
-            <img
-                src={asopImage}
-                alt=""
-            />
-        </div>
-    </section>
-);
+const DuesPaymentSuccess = () => {
+    const { user, userProfile } = UserAuth();
+    const [updateStatus, setUpdateStatus] = useState('');
 
-export default DuesPaymentSuccess
+    useEffect(() => {
+        if (userProfile) {
+            const updateMembership = async () => {
+                try {
+                    const userDocRef = doc(db, "user-profiles", user.uid);
+                    const currentDate = new Date();
+                    const memberUntilDate = new Date(currentDate.setFullYear(currentDate.getFullYear() + 1));
+                    await updateDoc(userDocRef,
+                        {memberUntil: memberUntilDate,
+                              paidDues: true}
+                );
+                } catch (error) {
+                    console.error("Error updating firebase: ", error);
+                    setUpdateStatus('Error updating member until date');
+                }
+            };
+            updateMembership().then(r => updateStatus);
+        }
+    }, [userProfile]);
+
+    if (!userProfile) {
+        return <div>Loading...</div>;
+    }
+    const { name } = userProfile;
+
+    return (
+        <section>
+            <h1>Thanks {name} For Paying Your Dues!</h1>
+            <div className={'align-center'}>
+                <img
+                    src={asopImage}
+                    alt=""
+                />
+            </div>
+        </section>
+    );
+};
+
+export default DuesPaymentSuccess;
