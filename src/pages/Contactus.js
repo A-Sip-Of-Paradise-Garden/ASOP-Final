97699003a7a89cd4c1e2f06ef29227549ebf86c2
jonathanvo18 <70184576+jonathanvo18@users.noreply.github.com>
2023-11-28 14:36:35 -0500

    Update Contactus.js

--- src/pages/Contactus.js
+++ src/pages/Contactus.js
@@ -1,9 +1,8 @@
 import React from "react";
-
 import email from "../assets/contact/email.png";
 import facebook from "../assets/contact/fb.png";
 import instagram from "../assets/contact/ig.png";
-//
+
 const Contact = () => {
   const emailLink =
   "mailto:asipofparadisegarden@gmail.com";
@@ -12,10 +11,7 @@ const Contact = () => {
   const instagramLink =
   "https://www.instagram.com/Asipofparadisegarden";
 
-
-const Contact = () => {
     return (
-
       <div className="flex flex-col items-center text-center gap-4">
       <h1 className="text-5xl font-bold">Contact Us</h1>
         Send us an email, follow us on social media, or get directions!
@@ -64,7 +60,7 @@ const Contact = () => {
           </a>
         </div>
       </div>
-      
+
       <div className="grid grid-cols-1 grid-rows-1 gap-4 border-2 border-emerald-400 rounded-xl p-4">
         <iframe 
           src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d3317.858194057881!2d-84.34714527283595!3d33.73848283423007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e0!4m3!3m2!1d33.7384805!2d-84.3444067!4m5!1s0x88f5015cbf3101fd%3A0xf5cd8a395657ddb0!2s572%20Stokeswood%20Ave%20SE%2C%20Atlanta%2C%20GA%2030316!3m2!1d33.7384449!2d-84.3444067!5e0!3m2!1sen!2sus!4v1697506734263!5m2!1sen!2sus" 
@@ -73,10 +69,27 @@ const Contact = () => {
           style={{border:"0"}} 
           allowfullscreen="" 
           loading="lazy" 
-        r eferrerpolicy="no-referrer-when-downgrade"
-      ></iframe></p>
-      </div>
-    )
-  }
+          referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+    </div>
+  );
+};
+
+const ContactCard = ({ src, alt, description, button = false }) => {
+  return (
+    <div
+      className={`flex flex-col overflow-normal border rounded-xl py-2 px-4 items-center gap-2 h-full justify-center ${
+        button ? "hover:bg-slate-100" : ""
+      }`}
+    >
+      <img src={src} alt={alt} className="max-w-[10rem] px-2" />
+      <p className={`${button ? "hidden" : ""} sm:block text-sm sm:text-base`}>
+        {description}
+      </p>
+    </div>
+  );
+};
+
   
   export default Contact;
